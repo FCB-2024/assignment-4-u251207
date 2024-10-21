1135ef64792def4efea7ade6ab60bc9bfe7bafa9
Martina Oviedo Lago <martina.oviedo01@estudiant.upf.edu>
2024-10-21 18:05:13 +0200

    Update antiprime.py

--- src/antiprime.py
+++ src/antiprime.py
@@ -17,14 +17,14 @@ def main(x) :
 		j = 1
 		k = 0
 		while j <= 1:
-			if 1 % j == 0:
+			if l % j == 0:
 				k = k + 1
 			j = j + 1
 		l = l - 1
 	if k >= c:
-		res = "not anti-prime"
+		res="not anti-prime"
 	else:
-		res = "anti-prime"
+		res="anti-prime"
 		
 
 
@@ -33,14 +33,15 @@ def main(x) :
 	## REPLACE THE FOLLOWING LINE BY WHATEVER LINES
 	## OF CODE ALLOW THIS FUNCTION TO RETURN THE VALUE
 	## "anti-prime" or "not anti-prime"
-	return("anti-prime")
+	return(res)
 
 ## DO NOT REMOVE THIS LINE BELOW
 if __name__ == "__main__" :
+	import sys
+	x=int(sys.argv[1])
+	print(main(x))
+	
 
-	## MODIFY THE LINE BELOW AND ADD BEFORE WHATEVER LINES ARE NECESSARY
-	## TO RUN THIS PROGRAM AS, FOR INSTANCE:
-	## $ python antiprime.py 6
 	## WHERE THE FIRST ARGUMENT IS A POSITIVE INTEGER NUMBER FOR WHICH
 	## YOU WANT TO FIGURE OUT WHETHER IS ANTI-PRIME OR NOT
 	print(main(x))
